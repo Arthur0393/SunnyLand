8e9a5b9a5fad7c0b4701fa9e6ab83e1388c24dca
Arthur0393 <aryou666.ra@hotmail.com>
2023-11-26 20:01:47 -0600

    Menu Pause

---
+++ Assets/Script/MenuPausa.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    public void update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(juegoPausado) {
+                Reanudar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+
+    public void Pausa()
+    {
+        juegoPausado = true;
+        Time.timeScale = 0f;
+        botonPausa .SetActive(false);
+        menuPausa .SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        Time .timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar()
+    {
+        Debug.Log("Cerrando Juego...");
+        Application.Quit();
+    }
+}
